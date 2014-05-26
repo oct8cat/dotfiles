6a3b65953978b16f6a3e8f4e34fb0c6ce5bbeb2a
Tako Neko <oct8cat@gmail.com>
2014-05-26 11:56:24 +0300

    Laptop is suffering. Battery widget.

--- dotfiles/.config/awesome/rc.lua
+++ dotfiles/.config/awesome/rc.lua
@@ -161,6 +161,18 @@ mytasklist.buttons = awful.util.table.join(
                                               if client.focus then client.focus:raise() end
                                           end))
 
+-- Battery
+batterywidget = wibox.widget.textbox()
+batterywidgettimer = timer({ timeout = 60 })
+function onTimeout()
+    fh = assert(io.popen("acpi | grep -o '[0-9]\\+%'", "r"))
+    batterywidget:set_text("| Bat: " .. fh:read("*l") .. " ")
+    fh:close()
+end
+onTimeout()
+batterywidgettimer:connect_signal("timeout", onTimeout)
+batterywidgettimer:start()
+
 for s = 1, screen.count() do
     -- Create a promptbox for each screen
     mypromptbox[s] = awful.widget.prompt()
@@ -191,6 +203,7 @@ for s = 1, screen.count() do
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
     right_layout:add(mytextclock)
+    right_layout:add(batterywidget)
     right_layout:add(mylayoutbox[s])
 
     -- Now bring it all together (with the tasklist in the middle)
